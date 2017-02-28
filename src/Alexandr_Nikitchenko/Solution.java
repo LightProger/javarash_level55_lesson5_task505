2b5e8206154b27cc85ce00391440e182fa667f21
Alexandr <photoluxe@mail.ru>
2017-02-28 13:43:35 +0600

    Битва котов

---
+++ src/Alexandr_Nikitchenko/Solution.java
@@ -0,0 +1,48 @@
+package Alexandr_Nikitchenko;
+
+/*
+Кошачья бойня
+Создать три кота используя класс Cat.
+Провести три боя попарно между котами.
+Класс Cat создавать не надо. Для боя использовать метод boolean fight(Cat anotherCat).
+Результат каждого боя вывести на экран c новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        // Создаем котов
+        Cat cat1 = new Cat("Васька", 7, 4, 15);
+        Cat cat2 = new Cat("Рыжик", 5, 3, 10);
+        Cat cat3 = new Cat("Соня", 3, 2, 5);
+
+        // Битва котов
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat1.fight(cat3));
+        System.out.println(cat2.fight(cat3));
+
+    }
+
+    public static class Cat {
+        protected String name;
+        protected int age;
+        protected int weight;
+        protected int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            int agePlus = this.age > anotherCat.age ? 1 : 0;
+            int weightPlus = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthPlus = this.strength > anotherCat.strength ? 1 : 0;
+
+            int score = agePlus + weightPlus + strengthPlus;
+            return score > 2; // return score > 2 ? true : false;
+        }
+    }
+}
